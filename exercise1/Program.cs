bfd16c2128cc27d46ab61be34129eff41184605a
Haikal <Haikal@DESKTOP-CP1H7ER>
2022-10-10 02:23:46 -0700

    Add project files.

---
+++ exercise1/Program.cs
@@ -0,0 +1,90 @@
+﻿ using System;
+
+namespace exercise1
+{
+    class Program
+    {
+        // Deklarasi array int dengan ukuran 06+10+20+20-30 = 26
+        private int[] dede = new int[26];
+        // Deklarasi variabel int untuk menyimpan banyaknya data pada array 
+        private int n;
+        // Fungsi / Method untuk menerima masukan 
+        public void read()
+        {
+            // Menerima angka untuk menentukan banyaknya data yang disimpan pada array 
+            while (true)
+            {
+                Console.Write("Masukkan banyaknya elemen pada Array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 26)
+                    break;
+                else
+                    Console.WriteLine("\nArray dapat mempunyai maksimal 26 elemen.\n");
+                Console.WriteLine("");
+                Console.WriteLine("------------------------");
+                Console.WriteLine(" Masukkan elemen array ");
+                Console.WriteLine("------------------------");
+                // Pengguna memasukkan elemen pada array
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("<" + (i + 1) + "> ");
+                    string s1 = Console.ReadLine();
+                    dede[i] = Int32.Parse(s1);
+                }
+
+            }
+        }
+
+        private void display()
+        {
+            // Menampilkan array yang tersusun
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine(" Element array yang telah tersusun");
+            Console.WriteLine("-----------------------------------");
+            for (int dc = 0; dc < n; dc++)
+            {
+                Console.WriteLine(dede[dc]++);
+            }
+            Console.WriteLine("");
+        }
+        public void BubleSortArray()
+        {
+            for (int i = 1; i < n; i++) // for n-1 passes
+            {
+                // Pada pass i , bandingkan n - i elemen pertama dengan elemen selanjutnya 
+                for (int dc = 0; dc < n - i; dc++)
+                {
+                    if (dede[dc] > dede[dc + 1]) // jika elemen tidak dalam urutan yang benar
+                    {
+                        //tukar elemen
+                        int temp;
+                        temp = dede[dc];
+                        dede[dc] = dede[dc + 1];
+                        dede[dc + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Creating the object of the Bubblesort class 
+            Program myList = new Program();
+            // Pemanggilan fungsi untuk menerima elemen array 
+            myList.read();
+            // Pemanggilan fungsi untuk menampilkan array 
+            myList.BubleSortArray();
+            //manggilan fungsi untuk menampilkan array yang tersusun
+            myList.display();
+            // Exit
+            Console.WriteLine("\n\nTekan Tombol Apa Saja Untuk Keluar. ");
+            Console.Read();
+
+        }
+    }
+}
+
+
+   
